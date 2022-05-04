27619e992117080a055f8b86c3c22b9423bfcdc5
Sultan Alsawaf <sultan@kerneltoast.com>
2022-05-03 23:21:44 -0700

    PM / freezer: Reduce freeze timeout to 1 second for Android
    Freezing processes on Android usually takes less than 100 ms, and if it
    takes longer than that to the point where the 20 second freeze timeout is
    reached, it's because the remaining processes to be frozen are deadlocked
    waiting for something from a process which is already frozen. There's no
    point in burning power trying to freeze for that long, so reduce the freeze
    timeout to a very generous 1 second for Android and don't let anything mess
    with it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -489,6 +489,9 @@ static ssize_t pm_debug_messages_store(struct kobject *kobj,
 {
 	unsigned long val;
 
+	if (IS_ENABLED(CONFIG_ANDROID))
+		return n;
+
 	if (kstrtoul(buf, 10, &val))
 		return -EINVAL;
 
