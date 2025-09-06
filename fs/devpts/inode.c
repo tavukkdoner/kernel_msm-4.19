9d75daf5d4a7d3fb3818f7021f5aaf5b7044e80c
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-09-06 07:31:31 +0300

    Update inode.c

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -629,9 +629,25 @@ void devpts_pty_kill(struct dentry *dentry)
 {
 	WARN_ON_ONCE(dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC);
 
+	pr_info("%s: dentry current: %px\n", __func__, dentry);
+    if(dentry->d_inode){
+	    pr_info("%s: dentry inode  current: %px\n", __func__, dentry->d_inode);
+    }
 	dentry->d_fsdata = NULL;
+	pr_info("%s: dentry1 current: %px\n", __func__, dentry);
+    if(dentry->d_inode){
+	    pr_info("%s: dentry1 inode  current: %px\n", __func__, dentry->d_inode);
+    }
 	drop_nlink(dentry->d_inode);
+	pr_info("%s: dentry2 current: %px\n", __func__, dentry);
+    if(dentry->d_inode){
+	    pr_info("%s: dentry2 inode  current: %px\n", __func__, dentry->d_inode);
+    }
 	d_delete(dentry);
+	pr_info("%s: dentry3 current: %px\n", __func__, dentry);
+    if(dentry->d_inode){
+	    pr_info("%s: dentry3 inode  current: %px\n", __func__, dentry->d_inode);
+    }
 	dput(dentry);	/* d_alloc_name() in devpts_pty_new() */
 }
 
