bfe52b0723457ad7db928f52f1e9b6e1a524dc79
Steve Sistare <steven.sistare@oracle.com>
2026-03-21 11:00:23 +0200

    sched/fair: Hoist idle_stamp up from idle_balance
    Move the update of idle_stamp from idle_balance to the call site in
    pick_next_task_fair, to prepare for a future patch that adds work to
    pick_next_task_fair which must be included in the idle_stamp interval.
    No functional change.
    
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4162,6 +4162,16 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 	rq->misfit_task_load = task_h_load(p);
 }
 
+static inline void rq_idle_stamp_update(struct rq *rq)
+{
+	rq->idle_stamp = rq_clock(rq);
+}
+
+static inline void rq_idle_stamp_clear(struct rq *rq)
+{
+	rq->idle_stamp = 0;
+}
+
 static void overload_clear(struct rq *rq)
 {
 	struct sparsemask *overload_cpus;
@@ -4207,6 +4217,8 @@ static inline int idle_balance(struct rq *rq, struct rq_flags *rf)
 	return 0;
 }
 
+static inline void rq_idle_stamp_update(struct rq *rq) {}
+static inline void rq_idle_stamp_clear(struct rq *rq) {}
 static inline void overload_clear(struct rq *rq) {}
 static inline void overload_set(struct rq *rq) {}
 
@@ -8542,7 +8554,15 @@ done: __maybe_unused;
 
 idle:
 	update_misfit_status(NULL, rq);
+	/*
+	 * We must set idle_stamp _before_ calling idle_balance(), such that we
+	 * measure the duration of idle_balance() as idle time.
+	 */
+	rq_idle_stamp_update(rq);
 	new_tasks = idle_balance(rq, rf);
+
+	if (new_tasks)
+		rq_idle_stamp_clear(rq);
 
 	/*
 	 * Because idle_balance() releases (and re-acquires) rq->lock, it is
@@ -12040,12 +12060,6 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 		return 0;
 
 	/*
-	 * We must set idle_stamp _before_ calling idle_balance(), such that we
-	 * measure the duration of idle_balance() as idle time.
-	 */
-	this_rq->idle_stamp = rq_clock(this_rq);
-
-	/*
 	 * Do not pull tasks towards !active CPUs...
 	 */
 	if (!cpu_active(this_cpu))
@@ -12143,9 +12157,6 @@ out:
 	/* Is there a task of a high priority class? */
 	if (this_rq->nr_running != this_rq->cfs.h_nr_running)
 		pulled_task = -1;
-
-	if (pulled_task)
-		this_rq->idle_stamp = 0;
 
 	rq_repin_lock(this_rq, rf);
 
