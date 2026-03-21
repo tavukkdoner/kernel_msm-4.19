61d0721b55626803fbab9d5e2f23c1d92b3abaa5
Steve Sistare <steven.sistare@oracle.com>
2026-03-21 10:16:32 +0200

    sched/topology: Provide cfs_overload_cpus bitmap
    Define and initialize a sparse bitmap of overloaded CPUs, per
    last-level-cache scheduling domain, for use by the CFS scheduling class.
    Save a pointer to cfs_overload_cpus in the rq for efficient access.
    
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -86,6 +86,7 @@
 
 struct rq;
 struct cpuidle_state;
+struct sparsemask;
 
 extern __read_mostly bool sched_predl;
 extern unsigned int sched_capacity_margin_up[NR_CPUS];
@@ -973,6 +974,7 @@ struct rq {
 	struct cfs_rq		cfs;
 	struct rt_rq		rt;
 	struct dl_rq		dl;
+	struct sparsemask	*cfs_overload_cpus;
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	/* list of leaf cfs_rq on this CPU: */
