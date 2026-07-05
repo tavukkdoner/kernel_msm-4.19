7f597bfe9b10cd684e4699380277c26a45c805e3
tavukkdoner <customromtester174@gmail.com>
2026-07-05 21:49:20 +0300

    fixup f2fs: Convert to XArray

--- fs/f2fs/verity.c
+++ fs/f2fs/verity.c
@@ -275,9 +275,7 @@ static void f2fs_merkle_tree_readahead(struct address_space *mapping,
 	struct blk_plug plug;
 
 	for (index = start_index; index < start_index + count; index++) {
-		rcu_read_lock();
-		page = radix_tree_lookup(&mapping->i_pages, index);
-		rcu_read_unlock();
+		page = xa_load(&mapping->i_pages, index);
 		if (!page || xa_is_value(page)) {
 			page = __page_cache_alloc(readahead_gfp_mask(mapping));
 			if (!page)
@@ -287,6 +285,7 @@ static void f2fs_merkle_tree_readahead(struct address_space *mapping,
 			nr_pages++;
 		}
 	}
+	
 	blk_start_plug(&plug);
 	f2fs_mpage_readpages(mapping, &pages, NULL, nr_pages, true);
 	blk_finish_plug(&plug);
